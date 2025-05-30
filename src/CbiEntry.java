edbda2edb8a83b04706f30a84f5280b533881e35
Claudio Brusaferri <brusacla@gmail.com>
2025-05-30 15:29:14 +0200

    Add files via upload

---
+++ src/CbiEntry.java
@@ -0,0 +1,12 @@
+public class CbiEntry {
+    public String description;
+    public String type;
+
+    public CbiEntry(String description, String type) {
+       this.description = description;
+        this.type = type;
+    }
+    public String getDescription() {
+        return description;
+    }
+}
