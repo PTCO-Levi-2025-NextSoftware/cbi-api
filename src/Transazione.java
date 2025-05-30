8944cb835f64c5447d7a781644ff2ab2c2febc0d
Claudio Brusaferri <brusacla@gmail.com>
2025-05-30 16:16:29 +0200

    Delete src directory

--- src/Transazione.java
+++
@@ -1,9 +0,0 @@
-public class Transazione {
-    public String descrizione;
-    public String causaleCbi;
-
-    public Transazione(String descrizione, String causaleCbi) {
-        this.descrizione = descrizione;
-        this.causaleCbi = causaleCbi;
-    }
-}
